40247fd42cb4d9077458163f63ab69ad7961d642
oksimveiti <semihcettin@gmail.com>
2025-12-09 19:34:43 -0800

    feat: Initial project setup with Java 21 and Spring Boot

---
+++ src/main/java/com/semihcetin/project/expensemanagerapi/ExpenseManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.semihcetin.project.expensemanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/semihcetin/project/expensemanagerapi/ExpenseManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihcetin.project.expensemanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
